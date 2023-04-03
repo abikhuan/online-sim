b11282aeed6ba202392eef0fae5e0c6aaeee3534
Avlay Khuanbay <avlay.khuanbay@erg.kz>
2023-04-03 14:14:56 +0600

    task #3

--- src/main/java/task/three/TaskNumberThree.java
+++ src/main/java/task/three/TaskNumberThree.java
@@ -2,39 +2,39 @@ package task.three;
 
 public class TaskNumberThree {
 
-//    void processTask(ChannelHandlerContext ctx) {
-//        InetSocketAddress lineAddress = new InetSocketAddress(getIpAddress(), getUdpPort());  CommandType typeToRemove;
-//        for (Command currentCommand : getAllCommands()) {
-//            if (currentCommand.getCommandType() == CommandType.REBOOT_CHANNEL) {
-//                sendCommand(ctx, lineAddress, currentCommand, () -> currentCommand.setSendDate(new Date()));
-//            } else {
-//                sendCommand(ctx, lineAddress, currentCommand, () -> currentCommand.incSendCounter());
-//            }
-//        }
-//        sendKeepAliveOkAndFlush(ctx);
-//    }
-//
-//
-//    void sendCommand(ChannelHandlerContext ctx, InetSocketAddress lineAddress, Command currentCommand, MarkCommand marker) {
-//        if (!currentCommand.isAttemptsNumberExhausted()) {
-//            sendCommandToContext(ctx, lineAddress, currentCommand.getCommandText());
-//            try {
-//                AdminController.getInstance().processUssdMessage(
-//                        new DblIncomeUssdMessage(lineAddress.getHostName(), lineAddress.getPort(), 0,
-//                                                    EnumGoip.getByModel(getGoipModel()), currentCommand.getCommandText()), false);
-//            } catch (Exception ignored) {
-//            }
-//            Log.ussd.write(String.format("sent: ip: %s; порт: %d; %s",
-//                    lineAddress.getHostString(), lineAddress.getPort(), currentCommand.getCommandText()));  currentCommand.setSendDate(new Date());
-//            marker.mark();
-//        } else {
-//            typeToRemove = currentCommand.getCommandType();
-//            deleteCommand(typeToRemove);
-//        }
-//    }
-//
-//    private interface MarkCommand {
-//        void mark();
-//    }
+    void processTask(ChannelHandlerContext ctx) {
+        InetSocketAddress lineAddress = new InetSocketAddress(getIpAddress(), getUdpPort());  CommandType typeToRemove;
+        for (Command currentCommand : getAllCommands()) {
+            if (currentCommand.getCommandType() == CommandType.REBOOT_CHANNEL) {
+                sendCommand(ctx, lineAddress, currentCommand, () -> currentCommand.setSendDate(new Date()));
+            } else {
+                sendCommand(ctx, lineAddress, currentCommand, () -> currentCommand.incSendCounter());
+            }
+        }
+        sendKeepAliveOkAndFlush(ctx);
+    }
+
+
+    void sendCommand(ChannelHandlerContext ctx, InetSocketAddress lineAddress, Command currentCommand, MarkCommand marker) {
+        if (!currentCommand.isAttemptsNumberExhausted()) {
+            sendCommandToContext(ctx, lineAddress, currentCommand.getCommandText());
+            try {
+                AdminController.getInstance().processUssdMessage(
+                        new DblIncomeUssdMessage(lineAddress.getHostName(), lineAddress.getPort(), 0,
+                                                    EnumGoip.getByModel(getGoipModel()), currentCommand.getCommandText()), false);
+            } catch (Exception ignored) {
+            }
+            Log.ussd.write(String.format("sent: ip: %s; порт: %d; %s",
+                    lineAddress.getHostString(), lineAddress.getPort(), currentCommand.getCommandText()));  currentCommand.setSendDate(new Date());
+            marker.mark();
+        } else {
+            typeToRemove = currentCommand.getCommandType();
+            deleteCommand(typeToRemove);
+        }
+    }
+
+    private interface MarkCommand {
+        void mark();
+    }
 
 }
